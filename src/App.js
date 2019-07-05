e27d8cf31fc5bc0fd8e44d00c2f8e0b43ce44dc8
Mikko Karvonen <mikkokarvonen@Mikko-MacBook-Pro.local>
2019-07-05 19:48:12 +0300

    Lisätty hahmot testiksi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Box = (props) => {
+    return <div className={`box ${props.class}`}>{props.face}</div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="boxContainer">
+        <Box class="box1" face="｡◕‿‿◕｡"/>
+        <Box class="box2" face="ಥ﹏ಥ"/>
+        <Box class="box3" face="ᵔᴥᵔ"/>
+        <Box class="box4" face="ಠ╭╮ಠ"/>
+        <Box class="box5" face="˘▾˘"/>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
