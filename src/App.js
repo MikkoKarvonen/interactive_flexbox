50da575d518af7cc94f2f9397ca97554db8b2832
Mikko Karvonen <mkarvonen34@gmail.com>
2019-07-09 17:48:35 +0300

    Lisätty mahdollisuus poistaa ja lisätä hahmoja

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './App.css';
 
 function App() {
+  const boxes = [{
+    class: "box1",
+    face: "｡◕‿‿◕｡"
+  },{
+    class: "box2",
+    face: "ಥ﹏ಥ"
+  },{
+    class: "box3",
+    face: "ᵔᴥᵔ"
+  },{
+    class: "box4",
+    face: "ಠ╭╮ಠ"
+  },{
+    class: "box5",
+    face: "˘▾˘"
+  }]
+
+  let [toShow, setToShow] = useState(3)
+
+  const editBoxes = (a) => {
+    setToShow(toShow+a)
+  }
+
   const Box = (props) => {
     return <div className={`box ${props.class}`}>{props.face}</div>
   }
@@ -9,11 +32,16 @@ function App() {
   return (
     <div className="App">
       <div className="boxContainer">
-        <Box class="box1" face="｡◕‿‿◕｡"/>
-        <Box class="box2" face="ಥ﹏ಥ"/>
-        <Box class="box3" face="ᵔᴥᵔ"/>
-        <Box class="box4" face="ಠ╭╮ಠ"/>
-        <Box class="box5" face="˘▾˘"/>
+        {boxes.map((item,i) => {
+          if (i < toShow){
+            return <Box key={i} class={item.class} face={item.face}/>
+          }
+          return null
+        })}
+      </div>
+      <div className="controls">
+        <button onClick={toShow < boxes.length ?()=> editBoxes(1) : null}>+</button>
+        <button onClick={toShow > 1 ?()=> editBoxes(-1) : null}>-</button>
       </div>
     </div>
   );
